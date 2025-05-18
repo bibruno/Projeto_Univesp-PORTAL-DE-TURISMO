ae5fef731225d6a5dceab1782731a145fef1f480
Bruno Bignoto <babignotto@gmail.com>
2025-05-17 23:42:55 -0300

    Update settings.py

--- turismo_interior/turismo_interior/settings.py
+++ turismo_interior/turismo_interior/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mma@zaosporji5+&s&^o26o(7stw@8y*g$q8esyfvsg%0jd22)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portal-turismo-interior-krvv.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
