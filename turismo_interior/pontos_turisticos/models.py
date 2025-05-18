aa0462894a9739309e86a4c61b88e2f9aefe32e6
Bruno Augusto Bignotto <babignotto@gmail.com>
2025-05-18 00:44:17 -0300

    Corrigindo modelo TouristSpot: adicionando campos faltantes e tornando alguns campos opcionais

--- turismo_interior/pontos_turisticos/models.py
+++ turismo_interior/pontos_turisticos/models.py
@@ -10,11 +10,13 @@ class Type(models.Model):
 
 class TouristSpot(models.Model):
     name = models.CharField(max_length=200)
-    city = models.ForeignKey('City', on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    city = models.CharField(max_length=100)
     address = models.TextField()
-    rating = models.FloatField()
-    place_id = models.CharField(max_length=100, unique=True)
-    types = models.ManyToManyField(Type)
+    rating = models.FloatField(null=True, blank=True)
+    place_id = models.CharField(max_length=100, unique=True, null=True, blank=True)
+    image = models.ImageField(upload_to='pontos_turisticos/', null=True, blank=True)
+    types = models.ManyToManyField(Type, blank=True)
 
     def __str__(self):
         return self.name
